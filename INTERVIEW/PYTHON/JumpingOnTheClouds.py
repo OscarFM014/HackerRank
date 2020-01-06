1b031e3c1184a09a7a16afc9a2f5875195048396
Oscar Fernández <oscar.gohan2010@hotmail.com>
2020-01-06 08:48:19 -0600

    finally

--- INTERVIEW/PYTHON/JumpingOnTheClouds.py
+++ INTERVIEW/PYTHON/JumpingOnTheClouds.py
@@ -1,21 +1,9 @@
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(lista):
-    pos = 0
-    second = 0
-    salto = 0
-    while second < len(lista):
-        if (second+2 < len(lista)):
-            if (lista[second+2] == 0):
-                pos = pos + 2
-                salto = salto + 1
-            else:
-                pos = pos +1
-                salto = salto + 1
-            second = pos
-    
-    return salto
+    search = 0;
+    jump = 0;
+    while len
 
-            
             
         
         
