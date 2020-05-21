764ef5439b404e24262ca3b50f40029fda6a2a59
Oscar Fernandez <oscar.gohan2010@hotmail.com>
2020-05-20 23:55:15 -0500

    sum

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first;
+    int second;
+    int sum;
+    first = 9;
+    second = 3;
+    sum = first + second;
+    cout << sum;
+}
