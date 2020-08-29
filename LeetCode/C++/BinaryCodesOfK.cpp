688de465177ecba9ed3c2e7c5fd1f2c95ce76f78
Oscar Fernandez <oscar.gohan2010@hotmail.com>
2020-08-28 23:32:51 -0500

    Binary Modified

--- LeetCode/C++/BinaryCodesOfK.cpp
+++ LeetCode/C++/BinaryCodesOfK.cpp
@@ -11,15 +11,12 @@ bool hasAllCodes(string s, int k)
         return false;
     }
     unordered_set<string> binary;
-
     for (int i = 0; i < s.size() - k + 1; i++)
     {
         binary.insert(s.substr(i, k));
     }
-
     int elements = binary.size();
     int realCombinations = pow(2, k);
-
     if (elements >= realCombinations)
     {
         return true;
