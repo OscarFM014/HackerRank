58ed59a767d5dc1fe8553d5ef6f4b2982e05d8c5
Oscar Fernandez <oscar.gohan2010@hotmail.com>
2021-01-11 22:50:45 -0600

    Move Zeroes :O

--- LeetCode/C++/Interview/Array/MoveZeroes.cpp
+++ LeetCode/C++/Interview/Array/MoveZeroes.cpp
@@ -1,3 +1,5 @@
+//Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -43,3 +45,5 @@ int main()
 
     return 0;
 }
+
+//Oscar
